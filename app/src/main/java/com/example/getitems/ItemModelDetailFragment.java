1211a5f0bfd569c081b3b769a7a7f561bab85cf9
cbrod <broderickcel@seattleu.edu>
2020-03-08 17:26:10 -0700

    Rename dummy directory to models

--- app/src/main/java/com/example/getitems/ItemModelDetailFragment.java
+++ app/src/main/java/com/example/getitems/ItemModelDetailFragment.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.example.getitems.dummy.ItemViewModel;
+import com.example.getitems.models.ItemViewModel;
 
 /**
  * A fragment representing a single ItemModel detail screen.
